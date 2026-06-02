ba65e24bf3c2848a707f32d1d644522c0585e8f0
Adryan Eryk de Oliveira Leite <adryanoliveira1928@gmail.com>
2026-06-02 09:22:57 -0300

    Add: const, iota and bitwise shift in GO

--- GO/Aprenda-GO/CAP-4/main.go
+++ GO/Aprenda-GO/CAP-4/main.go
@@ -5,8 +5,42 @@ import (
 	"runtime"
 )
 
+const (
+	_  = iota
+	KB = 1 << (iota * 10)
+	MB = 1 << (iota * 10)
+	GB
+	TB
+)
+
+const (
+	x int = 1
+	y int = iota
+	_
+	_
+	_
+	z 
+	
+)
+
 func main() {
+
+	fmt.Println("binary\t\t\t\tdecimal")
+	fmt.Printf("%b\t\t\t", KB)
+	fmt.Printf("%d\n", KB)
+	fmt.Printf("%b\t\t", MB)
+	fmt.Printf("%d\n", MB)
+	fmt.Printf("%b\t", GB)
+	fmt.Printf("%d\n", GB)
+
+
 	fmt.Println("GO Runtime")
 	fmt.Println(runtime.GOOS)
 	fmt.Println(runtime.GOARCH)
+
+	fmt.Printf("%v - %b - %X \n", x, x, x)
+	fmt.Printf("%v - %b - %X \n", x << 2, x << 2, x << 2)
+
+	fmt.Println(y)
+	fmt.Println(z)
 }
