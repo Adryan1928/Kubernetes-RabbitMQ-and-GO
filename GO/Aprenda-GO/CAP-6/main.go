0c891293dcc465ca07c17d145522c25958b2a704
Adryan Eryk de Oliveira Leite <adryanoliveira1928@gmail.com>
2026-06-06 09:26:14 -0300

    Add: CAP-6 og Go course

--- GO/Aprenda-GO/CAP-6/main.go
+++ GO/Aprenda-GO/CAP-6/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+var x interface{}
+
 func main() {
 	for x := 33; x <= 122; x++ {
 		fmt.Printf("%d - %U - %v \n", x, x, string(x))
@@ -15,5 +17,20 @@ func main() {
 		fmt.Println("True tbm")
 	} else {
 		fmt.Println("false")
+	}
+
+	x = 10
+
+	switch x.(type) {
+		case int:
+			fmt.Println("É um Int!")
+		case bool:
+			fmt.Println("É um Booleano!")
+		case string:
+			fmt.Println("É uma String!")
+		case float64:
+			fmt.Println("É um Float!")
+		default:
+			fmt.Println("Não tem tipo")
 	}
 }
