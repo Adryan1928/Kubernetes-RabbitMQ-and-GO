b89d49a5090d78d4f544099c93034b357a688591
Adryan Eryk de Oliveira Leite <adryanoliveira1928@gmail.com>
2026-06-20 19:43:44 -0300

    Fix: fix make in CAP-9 of GO course

--- GO/Aprenda-GO/CAP-9/main.go
+++ GO/Aprenda-GO/CAP-9/main.go
@@ -100,7 +100,7 @@ func Q5() {
 
 func Q6() {
 	fmt.Println("Question 6:")
-	slice := make([]string, 26, 26)
+	slice := make([]string, 26)
 	slice = []string{"Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"}
 	fmt.Printf("len: %d, cap: %d\n", len(slice), cap(slice))
 
